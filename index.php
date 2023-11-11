eea0c73b22f28d9775f10b2750bcada60dd40f20
Eric Minel <66842416+198905@users.noreply.github.com>
2023-11-11 12:44:49 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,32 @@
 <?php
 session_start();
-error_reporting(0);
-$erreur='';
-if(isset($_POST['pseudo']) && ($_POST['pseudo']!=='') ) {
-    $_SESSION['name']= htmlspecialchars(addslashes($_POST['pseudo']));
-    $id=$_POST['pseudo'];
-}else if(isset($_POST['pseudo'])&&($_POST['pseudo']=='')){
-    $erreur='LE PSEUDO!';
-    unset($_SESSION['pseudo']);
+require ('AltoRouter.php');
+$router = new AltoRouter();
+    if (isset($_POST['pseudo']) && ($_POST['pseudo'] !== '')) {
+        $_SESSION['name'] = htmlspecialchars(addslashes($_POST['pseudo']));
+        $id = $_POST['pseudo'];
+    }else if (isset($_POST['pseudo']) && ($_POST['pseudo'] == '')) {
+        "LE PSEUDO ON T'A DIT!";
+        unset($_SESSION['name']); // Corrected the session key
+    }
+
+$router->map('GET', '/id', function() { 
+    require __DIR__ . '/id.php';
+});
+
+$router->map('POST', '/main', function() {
+    if (isset($_POST['pseudo']) && ($_POST['pseudo'] == '')) {
+        require __DIR__ . '/id.php';
+    }else{
+        require __DIR__ . '/main.php';
+        require __DIR__ . '/model.php'; 
+    } 
+});
+$match = $router->match();
+
+// Dispatch the route
+if ($match && is_callable($match['target'])) {
+    call_user_func_array($match['target'], $match['params']);
+} else {
+    echo "Route not found!";
 }
-if (isset($_POST['sessionD'])) {
-    $_SESSION['name']='';
-    session_destroy();
-}
-require('main.php');
-require('model.php');
