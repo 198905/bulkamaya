5d730bcd02d3ee536c3ac5a5b5d0d8fe7960adea
Eric Minel <66842416+198905@users.noreply.github.com>
2023-11-20 18:27:47 +0300

    Update index.php

--- index.php
+++ index.php
@@ -5,28 +5,53 @@ $router = new AltoRouter();
     if (isset($_POST['pseudo']) && ($_POST['pseudo'] !== '')) {
         $_SESSION['name'] = htmlspecialchars(addslashes($_POST['pseudo']));
         $id = $_POST['pseudo'];
-    }else if (isset($_POST['pseudo']) && ($_POST['pseudo'] == '')) {
-        "LE PSEUDO ON T'A DIT!";
-        unset($_SESSION['name']); // Corrected the session key
+    }else if (isset($_POST['pseudo']) && ($_POST['pseudo'] == '') || (isset($_POST['sessionD']))) { //pb
+        unset($_SESSION['name']); 
     }
 
-$router->map('GET', '/id', function() { 
+$router->map('GET', '/', function() { 
     require __DIR__ . '/id.php';
 });
 
 $router->map('POST', '/main', function() {
-    if (isset($_POST['pseudo']) && ($_POST['pseudo'] == '')) {
+    if (isset($_POST['pseudo']) && ($_POST['pseudo'] == '') || (isset($_POST['sessionD']))) { //pb
+        unset($_SESSION['name']); 
         require __DIR__ . '/id.php';
-    }else{
-        require __DIR__ . '/main.php';
+        session_destroy();
+        header("Location: /id"); // Redirect to the home page or another appropriate location
+    exit();
+    }
+    if (isset($_POST['pseudo']) && ($_POST['pseudo'] !== '')) {
+        require __DIR__ . '/main.php';   
         require __DIR__ . '/model.php'; 
-    } 
+        }
 });
-$match = $router->match();
 
+$match = $router->match();
 // Dispatch the route
 if ($match && is_callable($match['target'])) {
     call_user_func_array($match['target'], $match['params']);
 } else {
-    echo "Route not found!";
+    echo "<h1>non.</h1>";
+    var_dump($_SESSION['name']);
+} 
+
+
+/*session_start();
+error_reporting(0);
+require('id.php');
+$erreur='';
+if(isset($_POST['pseudo']) && ($_POST['pseudo']!=='') ) {
+    $_SESSION['name']= htmlspecialchars(addslashes($_POST['pseudo']));
+    require('main.php');
+    require('model.php');
+    //var_dump($_SESSION, $_POST); MARCHE
+}else if(isset($_POST['pseudo'])&&($_POST['pseudo']=='')){
+    $erreur='LE PSEUDO!';
+    unset($_SESSION['pseudo']);
 }
+if (isset($_POST['sessionD'])) {
+    $_SESSION['name']='';
+    session_destroy();
+}*/
+
