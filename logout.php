43fc8cf74b1c15300ddbcc571ab6a81db47caee9
Eric Minel <66842416+198905@users.noreply.github.com>
2023-11-27 15:08:53 +0300

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Docnumet</title>
+</head>
+<body>
+    <form action="/id" method="post" name="logout">
+    <button type="submit" class="buttonModal"><h1>LOGOUT</h1></button>
+    </form>
+</body>
+</html>
