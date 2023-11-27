a5029a494ce822c537f1be0f1f0d60ee2ee0f1e7
Eric Minel <66842416+198905@users.noreply.github.com>
2023-11-27 15:12:43 +0300

    Update model.php

--- model.php
+++ model.php
@@ -1,23 +1,33 @@
 <?php
-session_start();
-ini_set('display_errors', 1);
+session_start(); 
+//fonctionne
 try {
-  $bdd = new PDO('mysql:host=localhost;dbname=databasemessages;charset=utf8', 
-  'root', '');
-  $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
+    $bdd = new PDO('mysql:host=localhost;dbname=databasemessages;charset=utf8', 'root', '');
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-  echo 'Connection failed: ' . $e->getMessage();
+    echo 'Connection failed: ' . $e->getMessage();
 }
 
 // Insert into the database
-if (isset($_POST['message']) && ($_POST['message'] !== '')) {
-  file_put_contents('request.json', json_encode($_POST, true));
-  $stmt = $bdd->prepare('INSERT INTO `messagestable` (`msg`, `pseudo`) VALUES (:msg, :pseudo)');
-  $msg = strip_tags(addslashes(htmlspecialchars($_POST["message"])));
-  $pseudo = $_SESSION['name'];
+if (isset($_POST['message']) && $_POST['message'] !== '') {
+    file_put_contents('request.json', json_encode($_POST, true)); 
+    
+    $msg = $_POST["message"];
+    $pseudo = $_SESSION['name'] ; 
+    
+    try {
+        $stmt = $bdd->prepare('INSERT INTO `messagestable` (`msg`, `pseudo`) VALUES (:msg, :pseudo)');
+        $stmt->bindValue(':msg', $msg);
+        $stmt->bindValue(':pseudo', $pseudo);
+        $success = $stmt->execute();
 
-  $stmt->bindValue(':msg', $msg);
-  $stmt->bindValue(':pseudo', $pseudo);
-  $success = $stmt->execute();
+        if ($success) {
+            echo "/";
+        } else {
+            echo "|";
+            print_r($stmt->errorInfo()); 
+        }
+    } catch (PDOException $e) {
+        echo "Error: " . $e->getMessage();
+    }
 }
